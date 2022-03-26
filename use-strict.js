4c115dad93443cda1fb83ff9b01914c3cbd9d4a5
Juan <cibensoft@gmail.com>
2022-03-26 09:39:00 -0300

    Se modifica comentario

--- use-strict.js
+++ use-strict.js
@@ -3,7 +3,7 @@
 miEdad = 45;
 console.log(`Mi edad es de $${miEdad} años`);
 
-// Si ejecuto este archivo en modo estricto, la ejecucion provocara un error dado que la variable "miVariable"
+// Si ejecuto este archivo en modo estricto, la ejecucion provocara un error dado que la variable "miEdad"
 // no fue definida.
 // Si ejecuto el mismo codigo pero en modo normal, es decir comentando la linea 'use strict', el codigo se ejecuta
 // sin problemas
