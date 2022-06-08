9a4f1d82e2059092cb3c46f6052223b758c48b63
Juan <juan.alesandri.acc@gmail.com>
2022-06-08 09:23:03 -0300

    Explicacion sobre como eliminnar un atributo de un objeto

--- delete-attribute-from-object.js
+++ delete-attribute-from-object.js
@@ -8,6 +8,20 @@ let merlin = {
 };
 
 let {hobbies, height, eyes, ...modifiedMerlin} = merlin;
+// Hola Juan.
+
+// Si utilizamos la desestructuración puedes crear un objeto con los elementos restantes al utilizar el operador spread.
+
+// En este caso:
+
+// const {google, ...newObject} = req.body:
+// utilizamos el operador spread para crear el objeto "newObject" con todos los elementos excepto "google".
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+
+// De igual manera, puedes crear un objeto sin la propiedad que deseas omitir.
+
+// Espero sea de ayuda. Saludos.
 
 //tambien se puede hacer esto para eliminar atributos de un objeto
 const modifiedMerlin2 = merlin;
